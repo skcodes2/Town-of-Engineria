fc9d3e1d2c365f1a3b863ce232d7d72f8552d9b8
SethLangendoen <slangend@uwo.ca>
2023-03-09 11:26:26 -0500

    yes

---
+++ PlayerCharacter.py
@@ -0,0 +1,12 @@
+class PlayerCharacter: 
+    def __init__(self,speed,health,money,buildingMaterials,currentLocation):
+        self.speed = speed
+        self.health = health
+        self.money = money
+        self.buildingMaterials = buildingMaterials
+        self.currentLocatin = currentLocation
+
+    def move(): 
+
+    
+        
