bd755a646a520f001f80342530d7260599649b0d
Raymond Li <rli684@uwo.ca>
2023-03-09 17:10:29 -0500

    new file:   icecube.png 	new file:   iceplatform.png 	new file:   largeplatform.png 	new file:   leftcornerplatform.png 	modified:   main.py 	new file:   mediumplatform.png 	new file:   rightcornerplatform.png 	new file:   smallplatform.png

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import pygame
 import GameObject
 
-
 pygame.init()
 
 backgroundImage_LvL1 = pygame.image.load('lvl1background.png')
-#backgroundImage_LvL2 = pygame.image.load("lvl2background.png")
+backgroundImage_LvL2 = pygame.image.load("lvl2background.png")
+backgroundImage_LvL3 = pygame.image.load("lvl3background.png")
 backgroundImage_Lvl1_rect = backgroundImage_LvL1.get_rect()
+backgroundImage_LvL2_rect = backgroundImage_LvL2.get_rect()
+backgroundImage_LvL3_rect = backgroundImage_LvL3.get_rect()
 
-
-screen = pygame.display.set_mode(
-    (backgroundImage_Lvl1_rect.width, backgroundImage_Lvl1_rect.height))
+screen = pygame.display.set_mode((backgroundImage_Lvl1_rect.width, backgroundImage_Lvl1_rect.height))
 screen_rect = screen.get_rect()
 
 def renderLevel1(): 
     screen.blit(backgroundImage_LvL1, backgroundImage_Lvl1_rect)
     pygame.display.flip()
 def renderLvl2():
-    pass
+    screen.blit(backgroundImage_LvL2, backgroundImage_LvL2_rect)
+    pygame.display.flip()
 def renderBossLvl(): 
-    pass
+    screen.blit(backgroundImage_LvL3, backgroundImage_LvL3_rect)
+    pygame.display.flip()
+
 running = True
 # gameloop
 while running:
@@ -29,4 +32,5 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        
 pygame.quit()
