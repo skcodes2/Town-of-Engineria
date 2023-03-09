b427033b67db57fe2d72d618e09944603d4a8377
EricTan <etan47@uwo.ca>
2023-03-09 12:19:37 -0500

    bruh

--- GameObject.py
+++ GameObject.py
@@ -6,3 +6,55 @@ class GameObject(pygame.sprite.Sprite):
         self.image = pygame.image.load(img_path)
         self.rect = self.image.get_rect()  
         self.rect.center = (x,y)
+
+class Bullet(GameObject):
+    def __init__(self, speed, damage, direction, x, y):
+        super.__init__(x,y,"Bullet.png")
+        self.speed = speed
+        self.damage = damage
+        self.direction = direction
+
+class SpeechBubble(GameObject):
+    def __init__(self,text, x, y):
+        self.text = text
+        super.__init__(x,y,"SpeechBubble.png")
+    
+    def replaceText(self, text):
+        self.text = text
+    
+    def closeBubble(self):
+        self.text = ""
+
+class Character(GameObject): 
+    def __init__(self,speed,health,currentLocation, x, y):
+        super.__init__(x,y,"Character.png")
+        self.speed = speed
+        self.health = health
+        self.currentLocation = currentLocation
+
+    def loseHp(self,damage): 
+        self.health = self.health - damage
+
+class Enemy:
+    def __init__(self, type, direction, health, x, y):
+        self.type = type
+        self.direction = direction
+        self.health = health
+        super.__init__(x,y,"Enemy.png")
+    
+    def loseHp(self, damage):
+        self.health = self.health - damage
+
+    def shoot(self):
+        speed = ""
+        damage = ""
+        if type == 1:
+            speed = 5
+            damage = 1
+        elif type == 2:
+            speed = 10
+            damage = 2
+        elif type == 3:
+            speed = 15
+            damage = 3
+        # return a bullet object with correct direction and starting position with given speed
