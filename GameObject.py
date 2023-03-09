7aa67e1b6ea3184f87f3a928252e23a43ad4b0f4
Sarabjot <skahlo4@gmail.com>
2023-03-09 18:27:30 -0500

    added Platforms and reOranized Files

--- GameObject.py
+++ GameObject.py
@@ -1,47 +1,54 @@
 import pygame
 
+
 class GameObject(pygame.sprite.Sprite):
-    def __init__(self,x,y,img_path):
+    def __init__(self, x, y, img_path):
         super().__init__()
         self.image = pygame.image.load(img_path)
-        self.rect = self.image.get_rect()  
-        self.rect.center = (x,y)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.x = x
+        self.y = y
+
 
 class Bullet(GameObject):
     def __init__(self, speed, damage, direction, x, y):
-        super.__init__(x,y,"Bullet.png")
+        super.__init__(x, y, "Bullet.png")
         self.speed = speed
         self.damage = damage
         self.direction = direction
 
+
 class SpeechBubble(GameObject):
-    def __init__(self,text, x, y):
+    def __init__(self, text, x, y):
         self.text = text
-        super.__init__(x,y,"SpeechBubble.png")
-    
+        super.__init__(x, y, "SpeechBubble.png")
+
     def replaceText(self, text):
         self.text = text
-    
+
     def closeBubble(self):
         self.text = ""
 
-class Character(GameObject): 
-    def __init__(self,speed,health,currentLocation, x, y):
-        super.__init__(x,y,"Character.png")
+
+class Character(GameObject):
+    def __init__(self, speed, health, currentLocation, x, y):
+        super.__init__(x, y, "Character.png")
         self.speed = speed
         self.health = health
         self.currentLocation = currentLocation
 
-    def loseHp(self,damage): 
+    def loseHp(self, damage):
         self.health = self.health - damage
+
 
 class Enemy(GameObject):
     def __init__(self, type, direction, health, x, y):
         self.type = type
         self.direction = direction
         self.health = health
-        super.__init__(x,y,"Enemy.png")
-    
+        super.__init__(x, y, "Enemy.png")
+
     def loseHp(self, damage):
         self.health = self.health - damage
 
@@ -58,3 +65,8 @@ class Enemy(GameObject):
             speed = 15
             damage = 3
         # return a bullet object with correct direction and starting position with given speed
+
+
+class PlatForms(GameObject):
+    def __init__(self, x, y, img_path):
+        super().__init__(x, y, img_path)
--- main.py
+++ main.py
@@ -3,34 +3,47 @@ import GameObject
 
 pygame.init()
 
-backgroundImage_LvL1 = pygame.image.load('lvl1background.png')
-backgroundImage_LvL2 = pygame.image.load("lvl2background.png")
-backgroundImage_LvL3 = pygame.image.load("lvl3background.png")
+backgroundImage_LvL1 = pygame.image.load('backgroundImages/lvl1background.png')
+backgroundImage_LvL2 = pygame.image.load("backgroundImages/lvl2background.png")
+backgroundImage_LvL3 = pygame.image.load("backgroundImages/lvl3background.png")
 backgroundImage_Lvl1_rect = backgroundImage_LvL1.get_rect()
 backgroundImage_LvL2_rect = backgroundImage_LvL2.get_rect()
 backgroundImage_LvL3_rect = backgroundImage_LvL3.get_rect()
 
-screen = pygame.display.set_mode((backgroundImage_Lvl1_rect.width, backgroundImage_Lvl1_rect.height))
+platForm_group = pygame.sprite.Group()
+platForm_group.add(GameObject.PlatForms(
+    0, 720, "platformImages/largeplatform.png"))
+platForm_group.add(GameObject.PlatForms(
+    1250, 720, "platformImages/iceplatform.png"))
+
+screen = pygame.display.set_mode(
+    (backgroundImage_Lvl1_rect.width, backgroundImage_Lvl1_rect.height))
 screen_rect = screen.get_rect()
 
-def renderLevel1(): 
+
+def renderLevel1():
     screen.blit(backgroundImage_LvL1, backgroundImage_Lvl1_rect)
+    platForm_group.draw(screen)
     pygame.display.flip()
+
+
 def renderLvl2():
     screen.blit(backgroundImage_LvL2, backgroundImage_LvL2_rect)
     pygame.display.flip()
-def renderBossLvl(): 
+
+
+def renderBossLvl():
     screen.blit(backgroundImage_LvL3, backgroundImage_LvL3_rect)
     pygame.display.flip()
+
 
 running = True
 # gameloop
 while running:
-    screen.blit(backgroundImage_LvL1, backgroundImage_Lvl1_rect)
-    pygame.display.flip()
+    renderLevel1()
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        
+
 pygame.quit()
