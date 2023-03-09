6a928cd5fb9de5f0ddbccbcb1b8058915f3ce0b5
SethLangendoen <slangend@uwo.ca>
2023-03-09 12:49:24 -0500

    k

--- Bullet.py
+++
@@ -1,8 +0,0 @@
-import pygame, GameObject
-
-class Bullet(GameObject):
-    def __init__(self, speed, damage, direction, x, y):
-        super.__init__(x,y,"Bullet.png")
-        self.speed = speed
-        self.damage = damage
-        self.direction = direction
