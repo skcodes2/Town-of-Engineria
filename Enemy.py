93bc3128a6ae522869068e6564f3da2d05eadb3f
EricTan <etan47@uwo.ca>
2023-03-09 11:34:39 -0500

    bruh

--- Enemy.py
+++ Enemy.py
@@ -1,2 +1,9 @@
-class Enemy(Character):
+class Enemy:
+    type = ""
+    health = ""
+    def __init__(self, type, health):
+        self.type = type
+        self.health = health
     
+    def loseHp(self, damage):
+        self.health = self.health - damage
