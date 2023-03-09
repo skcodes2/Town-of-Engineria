71f54ff08fc7809b90fe9da11c0ecbbd08cf12af
EricTan <etan47@uwo.ca>
2023-03-09 11:42:29 -0500

    bullet2

--- Enemy.py
+++ Enemy.py
@@ -1,3 +1,5 @@
+import Bullet
+
 class Enemy:
     type = ""
     health = ""
@@ -7,3 +9,17 @@ class Enemy:
     
     def loseHp(self, damage):
         self.health = self.health - damage
+
+    def shoot(self):
+        speed = ""
+        damage = ""
+        if type == 1:
+            speed = 5
+            damage = 1
+        elif type == 2:
+            speed = 10
+            damage = 2
+        elif type == 3:
+            speed = 15
+            damage = 3
+        return Bullet(speed, damage)
