a153d582cfb29ba05a2862eae80b6e042e63a5d1
Raymond Li <rli684@uwo.ca>
2023-03-13 08:42:20 -0400

    modified:   GameObject.py 	modified:   __pycache__/GameObject.cpython-39.pyc 	modified:   main.py

--- GameObject.py
+++ GameObject.py
@@ -9,7 +9,6 @@ class GameObject(pygame.sprite.Sprite):
         self.rect.topleft = (x, y)
         self.x = x
         self.y = y
-        
 
 
 class Bullet(GameObject):
@@ -33,50 +32,54 @@ class SpeechBubble(GameObject):
 
 
 class Character(GameObject):
-    def __init__(self, speed, health, x, y,image_path,screen):
+    def __init__(self, speed, health, x, y, image_path, screen):
         super().__init__(x, y, image_path)
         self.speed = speed
-        self.jumpingSpeed = 10
+        self.jumpingSpeed = 12
         self.health = health
-        self.currentPosition = [x,y]
+        self.currentPosition = [x, y]
 
         self.nexImage = 0
         self.screen = screen
         self.walkingR = pygame.image.load("animationImages/walkingR.png")
         self.walkingL = pygame.image.load("animationImages/walkingL.png")
-        self.standing = pygame.image.load("bobby.png")
+        self.standing = pygame.image.load("characterImages/bobby.png")
         self.inAir = False
 
     def loseHp(self, damage):
         self.health = self.health - damage
-        
-    def playerMovementControl(self,event):
-       
+
+    def playerMovementControl(self, event):
+
         if all(key == 0 for key in pygame.key.get_pressed()):
-            self.screen.blit(self.standing,tuple(self.currentPosition),(0,0,100,76) )
+            self.screen.blit(self.standing, tuple(
+                self.currentPosition), (0, 0, 100, 76))
         elif event[pygame.K_RIGHT]:
-            self.screen.blit(self.walkingR,tuple(self.currentPosition),(101*self.nexImage,0,100,76))
-            self.currentPosition[0]+=self.speed
-            self.nexImage +=1
-            if(self.nexImage==3):
-                self.nexImage=0
-        elif event[pygame.K_LEFT]: 
-            self.screen.blit(self.walkingL,tuple(self.currentPosition),(101*self.nexImage,0,95,76))
-            self.currentPosition[0]-=self.speed
-            self.nexImage +=1
-            if(self.nexImage==3):
-                self.nexImage=0
+            self.screen.blit(self.walkingR, tuple(
+                self.currentPosition), (101*self.nexImage, 0, 100, 76))
+            self.currentPosition[0] += self.speed
+            self.nexImage += 1
+            if(self.nexImage == 3):
+                self.nexImage = 0
+        elif event[pygame.K_LEFT]:
+            self.screen.blit(self.walkingL, tuple(
+                self.currentPosition), (101*self.nexImage, 0, 95, 76))
+            self.currentPosition[0] -= self.speed
+            self.nexImage += 1
+            if(self.nexImage == 3):
+                self.nexImage = 0
 
         if self.inAir is False and event[pygame.K_UP]:
             self.inAir = True
         if self.inAir is True:
-            self.screen.blit(self.standing,tuple(self.currentPosition),(0,0,100,76))
-            self.currentPosition[1] -=  self.jumpingSpeed
-            self.jumpingSpeed -=1
-            if self.jumpingSpeed <-10:
+            self.screen.blit(self.standing, tuple(
+                self.currentPosition), (0, 0, 100, 76))
+            self.currentPosition[1] -= self.jumpingSpeed
+            self.jumpingSpeed -= 1
+            if self.jumpingSpeed < -10:
                 self.inAir = False
-                self.jumpingSpeed =10        
-        
+                self.jumpingSpeed = 10
+
 
 class Enemy(GameObject):
     def __init__(self, type, direction, health, x, y):
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import GameObject
 
 pygame.init()
 
+pygame.display.set_caption("Bobby: The Town of Enginerea")
+pygame.display.set_icon(pygame.image.load("gameicon.png"))
 backgroundImage_LvL1 = pygame.image.load('backgroundImages/lvl1background.png')
 backgroundImage_LvL2 = pygame.image.load("backgroundImages/lvl2background.png")
 backgroundImage_LvL3 = pygame.image.load("backgroundImages/lvl3background.png")
@@ -11,22 +13,41 @@ backgroundImage_LvL2_rect = backgroundImage_LvL2.get_rect()
 backgroundImage_LvL3_rect = backgroundImage_LvL3.get_rect()
 
 platForm_group = pygame.sprite.Group()
-#floor platform 
-platForm_group.add(GameObject.PlatForms(0, 500, "lvl1platformImages/largebrownstone.png"))
-platForm_group.add(GameObject.PlatForms(324, 550, "lvl1platformImages/largebrownstone.png"))
-platForm_group.add(GameObject.PlatForms(648, 550, "lvl1platformImages/largebrownstone.png"))
-platForm_group.add(GameObject.PlatForms(972, 550, "lvl1platformImages/largebrownstone.png"))
-#sky platforms
-platForm_group.add(GameObject.PlatForms(987, 337, "lvl1platformImages/mediumorangestone.png"))
-platForm_group.add(GameObject.PlatForms(300, 360, "lvl1platformImages/leftcornerbrownstone.png"))
-platForm_group.add(GameObject.PlatForms(340, 360, "lvl1platformImages/rightcornerbrownstone.png"))
+# floor platform
+platForm_group.add(GameObject.PlatForms(
+    0, 500, "lvl1platformImages/largebrownstone.png"))
+platForm_group.add(GameObject.PlatForms(
+    324, 550, "lvl1platformImages/largebrownstone.png"))
+platForm_group.add(GameObject.PlatForms(
+    648, 550, "lvl1platformImages/largebrownstone.png"))
+platForm_group.add(GameObject.PlatForms(
+    972, 550, "lvl1platformImages/largebrownstone.png"))
+# orange platforms
+platForm_group.add(GameObject.PlatForms(
+    987, 337, "lvl1platformImages/mediumorangestone.png"))
+# sky platform 1
+platForm_group.add(GameObject.PlatForms(
+    135, 430, "lvl1platformImages/leftcornerbrownstone.png"))
+platForm_group.add(GameObject.PlatForms(
+    175, 430, "lvl1platformImages/rightcornerbrownstone.png"))
+# sky platform 2
+platForm_group.add(GameObject.PlatForms(
+    290, 360, "lvl1platformImages/leftcornerbrownstone.png"))
+platForm_group.add(GameObject.PlatForms(
+    330, 360, "lvl1platformImages/rightcornerbrownstone.png"))
+# brown platform
+platForm_group.add(GameObject.PlatForms(
+    450, 446, "lvl1platformImages/smallbrownstone.png"))
+platForm_group.add(GameObject.PlatForms(
+    450, 350, "lvl1platformImages/smallbrownstone.png"))
 
-screen = pygame.display.set_mode((backgroundImage_Lvl1_rect.width, backgroundImage_Lvl1_rect.height))
+screen = pygame.display.set_mode(
+    (backgroundImage_Lvl1_rect.width, backgroundImage_Lvl1_rect.height))
 screen_rect = screen.get_rect()
 
-#main character
-bobby = GameObject.Character(15,5,0,425,"bobby.png",screen)
-
+# main character
+bobby = GameObject.Character(
+    17, 5, 0, 425, "characterImages/bobby.png", screen)
 
 
 # rendering levels
@@ -34,26 +55,29 @@ def renderLevel1():
     screen.blit(backgroundImage_LvL1, backgroundImage_Lvl1_rect)
     platForm_group.draw(screen)
 
+
 def renderLvl2():
     screen.blit(backgroundImage_LvL2, backgroundImage_LvL2_rect)
     pygame.display.flip()
 
+
 def renderBossLvl():
     screen.blit(backgroundImage_LvL3, backgroundImage_LvL3_rect)
     pygame.display.flip()
+
 
 running = True
 # gameloop1
 while running:
     renderLevel1()
 
-    pygame.time.Clock().tick(18)
+    pygame.time.Clock().tick(14)
 
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        
+
     keys = pygame.key.get_pressed()
     bobby.playerMovementControl(keys)
     pygame.display.flip()
